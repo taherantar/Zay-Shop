0fd14a00526b8b24e62de53607e5300a5ef823af
taherantar <taher.hp@hotmail.com>
2023-05-12 01:24:33 +0300

    add file shop json

--- js/session.js
+++ js/session.js
@@ -1,10 +1,40 @@
-var person = {
-    name: "mohamed",
-    age: 27
-}
+var myOutput = document.getElementById("productsContainer");
+var products_req = new XMLHttpRequest();
 
-console.log(person)
-console.log(JSON.stringify(person));
+var myResponse = [];
 
+products_req.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        myResponse = JSON.parse(this.response);
+        myOutput.innerHTML = "";
+        for (let index = 0; index < myResponse.length; index++) {
+            const product = myResponse[index];
+            myOutput.innerHTML += `
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="${product.category.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.description}</h5>
+                        <p class="card-text">${product.title}</p>
+                        
+                        <ul class="list-unstyled d-flex justify-content-center mb-1">
+                        <li>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-warning fa fa-star"></i>
+                            <i class="text-muted fa fa-star"></i>
+                            <i class="text-muted fa fa-star"></i>
+                        </li>
+                    </ul>
+                    <p class="text-center mb-0">$250.00</p>
+                    </div>
+                </div>
+            </div>`;
 
-console.log("-".repeat(20));
+            console.log(product)
+        }
+    }
+};
+products_req.open("GET" , "data/person.json");
+// products_req.open("GET" , "http://api.escuelajs.co/api/v1/products");
+products_req.send();
